c32bc75508d204142e113483ff430806cd889e2d
Akil0814 <akil040814@gmail.com>
2026-02-11 17:22:16 -0800

    2/11

---
+++ 26/2/11.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+#include<iostream>
+int main()
+{
+    std::vector<int> v;
+    v.insert(v.end(),8);
+    v.insert(v.end(),2);
+    auto p=v.end();
+    auto q=p;
+    q--;
+    v.insert(q,3);
+    //*q=6;
+    //v.insert(p,7);
+    v.erase(q);
+
+
+    for(auto &iter:v)
+    {
+        std::cout<<iter<<" ";
+    }
+
+    return 0;
+}
